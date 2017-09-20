b0552a8246869c7d543ca3605877cfbd6eff17c7
GH-Zhou <zhou.gu@husky.neu.edu>
2017-09-20 02:12:49 -0400

    Lectures 27-31
    add auto layout constraints programmatically, layout the cards, and
    handle taps on the cards.

--- Match/Match/Card.swift
+++ Match/Match/Card.swift
@@ -12,21 +12,46 @@ class Card: UIView {
     
     let cardImageView = UIImageView()
     var cardValue = 0
+//    {
+//        didSet {
+//            // when any is set to cardValue, set the UIImage to the image that represents the value
+//            cardImageView.image = UIImage(named: cardNames[cardValue])
+//        }
+//    }
     let cardNames = ["card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "jack", "queen", "king", "ace"]
     
     // UIView, the superclass already has init() method
+    // Called when creating an object with code
     override init(frame: CGRect) {
         super.init(frame: frame) // reason for calling this: don't wanna lose class UIView's initialization
         
-        // custom functionality
-        // TODO: Initialize the image view with an image
-        // TODO: Add it to the view
-        // TODO: Set constraints to the image view
+        // Custom functionality
+        
+        // Add imageView into view
+        addSubview(cardImageView) // adding "view." will raise error because Card class is already a subclass of UIView
+        
+        // Initialize the image view with an image
+        cardImageView.translatesAutoresizingMaskIntoConstraints = false
+        cardImageView.image = UIImage(named: "back")
+        
+        // Set constraints to the UIImageView
+        let heightConstraint = NSLayoutConstraint(item: cardImageView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 170)
+        
+        let widthConstraint = NSLayoutConstraint(item: cardImageView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 120)
+        
+        cardImageView.addConstraints([heightConstraint, widthConstraint])
+        
+        let topConstraint = NSLayoutConstraint(item: cardImageView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 0) // self refers to Card class
+        
+        let leftConstraint = NSLayoutConstraint(item: cardImageView, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1, constant: 0)
+        
+        addConstraints([topConstraint, leftConstraint]) // call self
     }
     
     // If the following code is not added, an error will be raised:
     //    'required' initializer 'init(coder:)' must be provided by subclass of 'UIView'
     
+    // Called when creating an object with storyboard
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         // fatalError("init(coder:) has not been implemented")
@@ -40,5 +65,10 @@ class Card: UIView {
         // Drawing code
     }
     */
+    
+    
+    func flipUp() {
+        cardImageView.image = UIImage(named: cardNames[cardValue])
+    }
 
 }
--- Match/Match/ViewController.swift
+++ Match/Match/ViewController.swift
@@ -14,11 +14,73 @@ class ViewController: UIViewController {
     
     var cards = [Card]() // card array can be accessed in this class SCOPE
 
+    var stackViewArray = [UIStackView]()
+    @IBOutlet weak var firstStackView: UIStackView!
+    @IBOutlet weak var secondStackView: UIStackView!
+    @IBOutlet weak var thirdStackView: UIStackView!
+    @IBOutlet weak var fourthStackView: UIStackView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        // Put the four stackViews into an array
+        stackViewArray += [firstStackView, secondStackView, thirdStackView, fourthStackView]
+        
+        // Get the cards
         cards = model.getCards()
+        
+        // Layout the cards
+        layoutCards()
+    }
+    
+    func layoutCards() {
+        
+        // Keep track of which card we're at
+        var cardIndex = 0
+        
+        // Loop through the four stackViews and put four cards into each
+        for stackView in stackViewArray {
+            
+            // Put four cards into the stackView
+            for _ in 0...3 {
+                
+                // Check if card exists at index before adding
+                if cardIndex < cards.count {
+                    
+                    // Set card that we're looking at
+                    let card = cards[cardIndex]
+                    card.translatesAutoresizingMaskIntoConstraints = false
+                    
+                    // Create a gesture recognizer and attach it to the card
+                    let recognizer = UITapGestureRecognizer(target: self, action: #selector(cardTapped(gestureRecognizer: )))
+                    card.addGestureRecognizer(recognizer)
+                    
+                    // Set the size of the card object
+                    let heightConstraint = NSLayoutConstraint(item: card, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 170)
+                    
+                    let widthConstraint = NSLayoutConstraint(item: card, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 120)
+                    
+                    card.addConstraints([heightConstraint, widthConstraint])
+                    
+                    // Put one card into the stackView
+                    stackView.addArrangedSubview(card)
+                    cardIndex += 1
+                }
+                
+
+            }
+        }
+    }
+    
+    func cardTapped(gestureRecognizer: UITapGestureRecognizer) {
+        
+        // Card is tapped
+        let card = gestureRecognizer.view as! Card // treat view as a Card object, but kind of dangerouse cause view is an optional UIView type
+        
+        // Reveal card
+        card.flipUp()
     }
 
     override func didReceiveMemoryWarning() {
