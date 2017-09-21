20abe78a6623ebe3735658e1191e4fad6c78987a
Guiheng Zhou <zhou.gu@husky.neu.edu>
2017-09-21 01:07:56 -0400

    Finish Match app

--- Match/Match/Card.swift
+++ Match/Match/Card.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class Card: UIView {
     
-    let cardImageView = UIImageView()
+    let backImageView = UIImageView()
+    let frontImageView = UIImageView()
+    
     var cardValue = 0
 //    {
 //        didSet {
@@ -18,7 +20,22 @@ class Card: UIView {
 //            cardImageView.image = UIImage(named: cardNames[cardValue])
 //        }
 //    }
-    let cardNames = ["card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "jack", "queen", "king", "ace"]
+    let cardNames = ["card1", "card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "card11", "card12", "card13"]
+    
+    var flippedUp = false
+    var isMatched = false {
+        didSet {
+            if isMatched {
+                // Remove the image from the cardImageView
+                
+                UIView.animate(withDuration: 1, delay: 1, options: .curveEaseOut, animations: {
+                    self.backImageView.alpha = 0
+                    self.frontImageView.alpha = 0
+                }, completion: nil)
+
+            }
+        }
+    }
     
     // UIView, the superclass already has init() method
     // Called when creating an object with code
@@ -28,25 +45,42 @@ class Card: UIView {
         // Custom functionality
         
         // Add imageView into view
-        addSubview(cardImageView) // adding "view." will raise error because Card class is already a subclass of UIView
+        addSubview(backImageView) // adding "view." will raise error because Card class is already a subclass of UIView
         
         // Initialize the image view with an image
-        cardImageView.translatesAutoresizingMaskIntoConstraints = false
-        cardImageView.image = UIImage(named: "back")
+        backImageView.translatesAutoresizingMaskIntoConstraints = false
+        backImageView.image = UIImage(named: "back")
         
-        // Set constraints to the UIImageView
-        let heightConstraint = NSLayoutConstraint(item: cardImageView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 170)
+        // Set constraints to the backImageView
+        applySizeConstraints(imageView: backImageView)
+        applyPositionConstraints(imageView: backImageView)
         
-        let widthConstraint = NSLayoutConstraint(item: cardImageView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 120)
+        // Add frontImageView into view
+        addSubview(frontImageView)
+        frontImageView.translatesAutoresizingMaskIntoConstraints = false
+        applySizeConstraints(imageView: frontImageView)
+        applyPositionConstraints(imageView: frontImageView)
         
-        cardImageView.addConstraints([heightConstraint, widthConstraint])
+    }
+    
+    func applySizeConstraints(imageView:UIImageView) {
         
-        let topConstraint = NSLayoutConstraint(item: cardImageView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 0) // self refers to Card class
+        let heightConstraint = NSLayoutConstraint(item: imageView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 170)
         
-        let leftConstraint = NSLayoutConstraint(item: cardImageView, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1, constant: 0)
+        let widthConstraint = NSLayoutConstraint(item: imageView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 120)
+        
+        imageView.addConstraints([heightConstraint, widthConstraint])
+    }
+    
+    func applyPositionConstraints(imageView:UIImageView) {
+        
+        let topConstraint = NSLayoutConstraint(item: imageView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 0) // self refers to Card class
+        
+        let leftConstraint = NSLayoutConstraint(item: imageView, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1, constant: 0)
         
         addConstraints([topConstraint, leftConstraint]) // call self
     }
+    
     
     // If the following code is not added, an error will be raised:
     //    'required' initializer 'init(coder:)' must be provided by subclass of 'UIView'
@@ -68,7 +102,28 @@ class Card: UIView {
     
     
     func flipUp() {
-        cardImageView.image = UIImage(named: cardNames[cardValue])
+        
+        // Set the front card image
+        frontImageView.image = UIImage(named: cardNames[cardValue])
+        
+        // Run the transition animation
+        UIView.transition(from: backImageView, to: frontImageView, duration: 1, options: .transitionFlipFromLeft, completion: nil)
+        
+        applyPositionConstraints(imageView: frontImageView)
+        
+        // Set the flag
+        flippedUp = true
+    }
+    
+    func flipDown() {
+        
+        // Run the transition animation
+        UIView.transition(from: frontImageView, to: backImageView, duration: 1, options: .transitionFlipFromLeft, completion: nil)
+        
+        applyPositionConstraints(imageView: backImageView)
+        
+        // Set the flag
+        flippedUp = false
     }
 
 }
--- Match/Match/ViewController.swift
+++ Match/Match/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     let model = CardModel() // create a new card model
-    
     var cards = [Card]() // card array can be accessed in this class SCOPE
+    var revealedCard:Card? // either contains a Card object or nothing; track if the first card is revealed
+    
+    var timer:Timer?
+    var countdown = 45
+    
+    var cardFlipSoundPlayer:AVAudioPlayer?
+    var correctSoundPlayer:AVAudioPlayer?
+    var wrongSoundPlayer:AVAudioPlayer?
+    var shuffleSoundPlayer:AVAudioPlayer?
 
     var stackViewArray = [UIStackView]()
     @IBOutlet weak var firstStackView: UIStackView!
@@ -20,10 +29,46 @@ class ViewController: UIViewController {
     @IBOutlet weak var thirdStackView: UIStackView!
     @IBOutlet weak var fourthStackView: UIStackView!
     
+    @IBOutlet weak var countdownLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Create and initialize the sound players
+        do {
+            
+            let shuffleSoundPath = Bundle.main.path(forResource: "shuffle", ofType: "wav") // app bundle
+            // An app bundle is the delivery vessel for an app. It contains the compiled code and resources in a
+            // neat little package that is used for installation on a device.
+            let shuffleSoundUrl = URL(fileURLWithPath: shuffleSoundPath!)
+            shuffleSoundPlayer = try AVAudioPlayer(contentsOf: shuffleSoundUrl) // may throw error
+            
+        } // if any error is thrown, go to catch
+        catch {
+            // Sound player couldn't be created
+        }
+        
+        do {
+            let cardFlipSoundPath = Bundle.main.path(forResource: "cardflip", ofType: "wav")
+            let cardFlipSoundUrl = URL(fileURLWithPath: cardFlipSoundPath!)
+            cardFlipSoundPlayer = try AVAudioPlayer(contentsOf: cardFlipSoundUrl)
+        }
+        catch { }
+        
+        do {
+            let correctSoundPath = Bundle.main.path(forResource: "dingcorrect", ofType: "wav")
+            let correctSoundUrl = URL(fileURLWithPath: correctSoundPath!)
+            correctSoundPlayer = try AVAudioPlayer(contentsOf: correctSoundUrl)
+        }
+        catch { }
+        
+        do {
+            let wrongSoundPath = Bundle.main.path(forResource: "dingwrong", ofType: "wav")
+            let wrongSoundUrl = URL(fileURLWithPath: wrongSoundPath!)
+            wrongSoundPlayer = try AVAudioPlayer(contentsOf: wrongSoundUrl)
+        }
+        catch { }
         
         // Put the four stackViews into an array
         stackViewArray += [firstStackView, secondStackView, thirdStackView, fourthStackView]
@@ -33,9 +78,65 @@ class ViewController: UIViewController {
         
         // Layout the cards
         layoutCards()
+        
+        // Set the countdown label
+        countdownLabel.text = String(countdown)
+        
+        // Create and schedule a timer
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerUpdate), userInfo: nil, repeats: true) // every second repeat until stopped
+    }
+    
+    func timerUpdate() {
+        countdown -= 1
+        
+        if countdown == 0 {
+            // Stop the timer
+            timer?.invalidate()
+            
+            // Stop the match game
+            
+            // Check if the user has matched all of the cards
+            var userHasMatchedAllCards = true
+            for card in cards {
+                if card.isMatched == false {
+                    userHasMatchedAllCards = false
+                    break
+                }
+            }
+            
+            var popUpMessage = ""
+            if userHasMatchedAllCards {
+                // Game is won
+                popUpMessage = "You won!"
+                
+            } else {
+                // Game is lost
+                popUpMessage = "You lost!"
+                
+            }
+            
+            // Create alert
+            let alert = UIAlertController(title: "Time's Up!", message: popUpMessage, preferredStyle: .alert)
+            
+            // Create alert action
+            let alertAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            
+            // Attach action to alert
+            alert.addAction(alertAction)
+            
+            present(alert, animated: true, completion: nil)
+            
+        }
+        
+        // Update the label in the UI
+        countdownLabel.text = String(countdown)
+        
     }
     
     func layoutCards() {
+        
+        // Play shuffleSound
+        shuffleSoundPlayer?.play()
         
         // Keep track of which card we're at
         var cardIndex = 0
@@ -76,11 +177,59 @@ class ViewController: UIViewController {
     
     func cardTapped(gestureRecognizer: UITapGestureRecognizer) {
         
+        // Check if countdown is zero
+        if countdown == 0 {
+            return
+        }
+        
         // Card is tapped
         let card = gestureRecognizer.view as! Card // treat view as a Card object, but kind of dangerouse cause view is an optional UIView type
         
-        // Reveal card
-        card.flipUp()
+        // Check if the card is already flipped up
+        if card.flippedUp == false {
+            
+            // Play cardFlipSound
+            cardFlipSoundPlayer?.play()
+            
+            // Reveal card
+            card.flipUp()
+            
+            if revealedCard == nil {
+                // First card being flipped up
+                
+                // Track this card as the first card being flipped up
+                revealedCard = card
+            } else {
+                // Second card being flipped up
+                // optional chaining, a safeguard; even revealedCard is nil, the program won't crash
+                if card.cardValue == revealedCard?.cardValue {
+                    // Cards match
+                    
+                    // Play correctSound
+                    correctSoundPlayer?.play()
+                    
+                    // remove both cards from the grid
+                    card.isMatched = true
+                    revealedCard?.isMatched = true
+                    
+                } else {
+                    // Cards don't match
+                    
+                    // Play wrongSound
+                    wrongSoundPlayer?.play()
+                    
+                    // Flip both of them down again
+                    let _ = Timer.scheduledTimer(timeInterval: 1, target: card, selector: #selector(Card.flipDown), userInfo: nil, repeats: false)
+                    
+                    let _ = Timer.scheduledTimer(timeInterval: 1, target: revealedCard!, selector: #selector(Card.flipDown), userInfo: nil, repeats: false)
+                    
+                }
+                
+                // Reset the tracking of the first card
+                revealedCard = nil
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
